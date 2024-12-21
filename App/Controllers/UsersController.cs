a2e49c8e4ce17998be2e82f69ff721c2c15b3dc6
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-12-21 13:31:59 +0530

    Implement user retrieval, update, and deletion endpoints; remove obsolete CreateUserRequest

--- App/Controllers/UsersController.cs
+++ App/Controllers/UsersController.cs
@@ -12,6 +12,20 @@ public class CreateUserController(UsersService usersService) : ControllerBase
 {
     private readonly UsersService _usersService = usersService;
 
+    [HttpGet("{userId:guid}")]
+    public IActionResult Get(Guid userId)
+    {
+        var user = _usersService.Get(userId);
+
+        return Ok(
+            new
+            {
+                message = "User Found",
+                user = UserResponse.FromDomain(user)
+            }
+        );
+    }
+
     [HttpPost]
     public IActionResult CreateUser(CreateUserRequest request)
     {
@@ -39,6 +53,35 @@ public class CreateUserController(UsersService usersService) : ControllerBase
             {
                 message = "User created successfully.",
                 user = UserResponse.FromDomain(user)
+            }
+        );
+    }
+
+    [HttpPut("{userId:guid}")]
+    public IActionResult Update(Guid userId, UpdateUserRequest request)
+    {
+        var user = request.ToDomain();
+
+        _usersService.Update(user, userId);
+
+        return Ok(
+            new
+            {
+                message = "User Updated",
+                user = UserResponse.FromDomain(user)
+            }
+        );
+    }
+
+    [HttpDelete("{userId:guid}")]
+    public IActionResult Delete(Guid userId)
+    {
+        _usersService.Delete(userId);
+
+        return Ok(
+            new
+            {
+                message = "User Deleted"
             }
         );
     }
